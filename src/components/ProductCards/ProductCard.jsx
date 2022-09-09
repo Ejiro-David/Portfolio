e41560ff38fb76a9a1ab563b78c1fc85eedae91c
EJ-David <daviderhabor.de@gmail.com>
2022-09-09 19:22:14 +0100

    tiny progress

--- src/components/ProductCards/ProductCard.jsx
+++ src/components/ProductCards/ProductCard.jsx
@@ -3,7 +3,12 @@ import "./ProductCard.css";
 
 function ProductCard() {
   return (
-    <div className='p'>Card</div>
+    <div className='p'>
+      <div className="p-pad"></div>
+      <a href='x' target='_blank'>
+        <img alt='' src='https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80'/>
+      </a>
+    </div>
   )
 }
 
